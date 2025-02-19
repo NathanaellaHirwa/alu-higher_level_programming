17baedb77cf93bb51a0cd225e9cb18661bcaae58
NathanaellaHirwa <n.hirwa@alustudent.com>
2025-02-19 09:43:17 -0800

    py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "object-oriented "
-str2 = "programming with "
-str3 = str1 + str2 + "Python"
-print(str3)
+str = "Python is an interpreted, interactive, object-oriented programming\
+    language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[-22:-17] + str[:6]
+print(str)
