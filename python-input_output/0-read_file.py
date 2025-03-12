57f4d4e03f7dee26ef7bfe9b288ea5c5ae268dc1
NathanaellaHirwa <n.hirwa@alustudent.com>
2025-03-12 02:19:01 -0700

    pio

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """
-Module to read and print the contents of a file to stdout.
+function that reads a text file
 """
 
 
 def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout
+    Returns none
     """
-    Reads a text file (UTF8) and prints it to stdout.
-
-    Args:
-        filename (str): The path to the file to read.
-    """
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read(), end="")
-
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
